ab1f7dc77f0d84c3db9298271e42c8b41caeb258
Hernan Oliva <haoliva@gmail.com>
2019-11-25 14:04:40 -0500

    First Commit

---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+
+  
+		for (int i = 0; i <=40; i++) {
+			//System.out.println("Hello students this you devops class..."+ i);
+			System.out.println("Change the content..."+ i);
+		}
+  
+	}
+
+}
